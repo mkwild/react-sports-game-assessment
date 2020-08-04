4652a546683627c6186e7b9d6deb8f13b6473a9d
Wolf <michael.wildnauer4481@gmail.com>
2020-08-04 14:41:11 -0400

    Updates

---
+++ precache-manifest.043a3ca0a6827206e9e7b8351d271546.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c36db70cce35a9b66d4088a77cce79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ed7364ac5b27588b81e",
+    "url": "/static/css/main.af216d45.chunk.css"
+  },
+  {
+    "revision": "83ec443dbaee11d6f020",
+    "url": "/static/js/2.fbb68554.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fbb68554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ed7364ac5b27588b81e",
+    "url": "/static/js/main.2c315ae1.chunk.js"
+  },
+  {
+    "revision": "c1882920c6cf7088a024",
+    "url": "/static/js/runtime-main.8655bd69.js"
+  },
+  {
+    "revision": "59f365b90752f50ac04f2b8b243de5e8",
+    "url": "/static/media/background.59f365b9.jpg"
+  },
+  {
+    "revision": "a26d4543e0224178273e0ed70894b07c",
+    "url": "/static/media/logo1.a26d4543.png"
+  },
+  {
+    "revision": "2594219ce5cd3f6f16f8af8e5109be59",
+    "url": "/static/media/logo2.2594219c.jfif"
+  },
+  {
+    "revision": "3776dfaf2df3ed64a92f90b63fa2c1b8",
+    "url": "/static/media/logo3.3776dfaf.jfif"
+  },
+  {
+    "revision": "6c66ad1afc430a31a12c42af92c89ccf",
+    "url": "/static/media/logo4.6c66ad1a.png"
+  },
+  {
+    "revision": "ec73a758b80561615126a80f98880234",
+    "url": "/static/media/miss.ec73a758.wav"
+  },
+  {
+    "revision": "1c5e2a77b58e97d13af72560d372d1e9",
+    "url": "/static/media/score.1c5e2a77.wav"
+  }
+]);
